aa38013a70f418ef7d2371b9f7accb0654d22b5c
Zelle Mandez <Zelle@zelles-macbook-pro.local>
2018-01-29 11:17:14 -0600

    Fixed merging error

--- Shotplotter/MatchView.swift
+++ Shotplotter/MatchView.swift
@@ -54,10 +54,10 @@ class MatchView {
                     if (double) {newNumber += 1}
                 }
                 var newColor = UIColor.black// >>>>Ignore what it tells you. Do NOT change this to a let!<<<<
-                players.append(Player.init(_number: newNumber, _color: newColor, _name: <#String#>))
+                players.append(Player.init(_number: newNumber, _color: newColor, _name: ""))
             }
-            var newColor = UIColor.black// >>>>Ignore what it tells you. Do NOT change this to a let!<<<<
-            players.append(Player.init(_number: newNumber, _color: newColor, _name: ""))
+            //var newColor = UIColor.black// >>>>Ignore what it tells you. Do NOT change this to a let!<<<<
+            //players.append(Player.init(_number: newNumber, _color: newColor, _name: ""))
         } else { // If a player is being removed
             while (players.count > newNum) {
                 players.removeLast()
