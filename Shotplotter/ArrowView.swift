773b47df06bab056bbd86290dd576b6c9e23aaaf
SOURISH KUNDU <778092@ehs-s-232-37.local>
2018-02-23 12:27:43 -0600

    everything is working as for right now after updating to swift 4

--- Shotplotter/ArrowView.swift
+++ Shotplotter/ArrowView.swift
@@ -18,7 +18,7 @@ class ArrowView: UIView {
     var startPoint: CGPoint
     var dispPath: UIBezierPath
     var storePath: UIBezierPath
-    var finPoint: CGPoint
+    var endPoint: CGPoint
     
     var dispLayer: CAShapeLayer
     var storeLayer: CAShapeLayer
@@ -35,7 +35,7 @@ class ArrowView: UIView {
         //endPoint = CGPoint()
         dispPath = UIBezierPath()
         storePath = UIBezierPath()
-        finPoint = CGPoint()
+        endPoint = CGPoint()
         dispLayer = CAShapeLayer()
         storeLayer = CAShapeLayer()
         
@@ -47,22 +47,33 @@ class ArrowView: UIView {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("Howdy I am touchesBegan begining")
         isDrawing = true
         guard let touch = touches.first else { return }
         startPoint = touch.location(in: self)
+        //touchesMoved(Set<UITouch>, with: <#T##UIEvent?#>)
+        print("Howdy I am touchesBegan end")
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("Howdy I am touchesEnded began")
         guard isDrawing else { return }
         isDrawing = false
         storePath.move(to: startPoint)
-        storePath.addLine(to: finPoint)
+        storePath.addLine(to: endPoint)
         drawShapeLayer(path: storePath, layer: storeLayer)
+        activePlayer.layer = self.storeLayer
+        print(activePlayer.number)
+        print(startPoint)
+        print(endPoint)
         dispPath.removeAllPoints()
-        activePlayer.addLine(start: startPoint, end: finPoint, tip: false, rotation: 0, slide: false, A: false, roll: false, hit: false, didScore: true)
+        activePlayer.addLine(start: startPoint, end: endPoint, tip: false, rotation: 0, slide: false, A: false, roll: false, hit: false, didScore: true)
+        endPoint = startPoint
+        print("Howdy I am touchesBegan end")
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("Howdy I am touchesMoved beganning")
         guard isDrawing else { return }
         guard let touch = touches.first else { return }
         let touchPoint = touch.location(in: self)
@@ -70,8 +81,9 @@ class ArrowView: UIView {
         dispPath.move(to: startPoint)
         dispPath.addLine(to: touchPoint)
         drawShapeLayer(path: dispPath, layer: dispLayer)
-        finPoint = touchPoint
+        endPoint = touchPoint
         drawShapeLayer(path: storePath, layer: storeLayer)
+        print("Howdy I am touchesMoved end")
     }
     
     func drawShapeLayer(path: UIBezierPath, layer: CAShapeLayer) {
