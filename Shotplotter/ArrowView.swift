eb2711a18eaf187c675f9f78845e49cf42d16c92
SOURISH KUNDU <778092@ehs-s-232-37.local>
2018-02-28 12:25:55 -0600

    broken drawing code

--- Shotplotter/ArrowView.swift
+++ Shotplotter/ArrowView.swift
@@ -10,38 +10,62 @@ import Foundation
 import UIKit
 
 class ArrowView: UIView {
+    
     var data: RotationView?
+    
     var color: UIColor
     var strokeWidth: CGFloat
-    
     var isDrawing: Bool
     var startPoint: CGPoint
-    var dispPath: UIBezierPath
+    var drawPath: UIBezierPath
     var storePath: UIBezierPath
     var endPoint: CGPoint
     
-    var dispLayer: CAShapeLayer
-    var storeLayer: CAShapeLayer
+    var drawLayer: CAShapeLayer
+    var storeLayer: [CAShapeLayer]
     
     var activePlayer: Player
     
+    private var index: Int
     
     required init?(coder aDecoder: NSCoder) {
-        color = UIColor.black
+        activePlayer = Player()
+        index = 0
+        color = UIColor.red
         strokeWidth = 3
         isDrawing = false
-        startPoint = CGPoint()
-        //endPoint = CGPoint()
-        dispPath = UIBezierPath()
+        drawPath = UIBezierPath()
         storePath = UIBezierPath()
+        drawLayer = CAShapeLayer()
+        startPoint = CGPoint()
         endPoint = CGPoint()
-        dispLayer = CAShapeLayer()
-        storeLayer = CAShapeLayer()
         
-        activePlayer = Player()
+        storeLayer = [CAShapeLayer]()
+        
+        
+        //interpolationPoints = [CGPoint]()
+        
         super.init(coder: aDecoder)
+        
         self.clipsToBounds = true
+        storeLayer.append(CAShapeLayer())
+        
+        layer.addSublayer(storeLayer[index])
+        layer.addSublayer(drawLayer)
+        
+        drawLayer.fillColor = nil
+        drawLayer.strokeColor = color.cgColor
+        drawLayer.lineWidth = strokeWidth
+        drawLayer.lineCap = kCALineCapRound
+        
+        storeLayer[index].fillColor = nil
+        storeLayer[index].strokeColor = color.cgColor
+        storeLayer[index].lineWidth = strokeWidth
+        storeLayer[index].lineCap = kCALineCapRound
+        
+        storeLayer[index].path = CGMutablePath()
     }
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         data?.checkDraw()
@@ -59,12 +83,12 @@ class ArrowView: UIView {
         storePath.move(to: startPoint)
         storePath.addLine(to: endPoint)
         data?.protoLine.endPos = endPoint
-        drawShapeLayer(path: storePath, layer: storeLayer)
-        activePlayer.layer = self.storeLayer
+        //drawShapeLayer(path: storePath, layer: storeLayer)
+        //activePlayer.layer = self.storeLayer
         //print(activePlayer.number)
         //print(startPoint)
         //print(endPoint)
-        dispPath.removeAllPoints()
+        drawPath.removeAllPoints()
         activePlayer.addLine(line: (data?.protoLine)!)
         endPoint = startPoint
         data?.protoLine.reset()
@@ -78,12 +102,12 @@ class ArrowView: UIView {
         guard isDrawing else { return }
         guard let touch = touches.first else { return }
         let touchPoint = touch.location(in: self)
-        dispPath.removeAllPoints()
-        dispPath.move(to: startPoint)
-        dispPath.addLine(to: touchPoint)
-        drawShapeLayer(path: dispPath, layer: dispLayer)
+        drawPath.removeAllPoints()
+        drawPath.move(to: startPoint)
+        drawPath.addLine(to: touchPoint)
+        drawShapeLayer(path: drawPath, layer: drawLayer)
         endPoint = touchPoint
-        drawShapeLayer(path: storePath, layer: storeLayer)
+        drawShapeLayer(path: storePath, layer: storeLayer[index])
     }
     
     func drawShapeLayer(path: UIBezierPath, layer: CAShapeLayer) {
@@ -97,7 +121,9 @@ class ArrowView: UIView {
     func changeColor(player: Player) {
         activePlayer = player
         self.color = player.getColor()
-        self.storeLayer = player.getLayer()
+        index = player.number
     }
+    
+    
 }
 
